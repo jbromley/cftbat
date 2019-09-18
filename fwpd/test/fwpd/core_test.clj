90ce34ab0a9787a62fc2e1fa26e46510fe9a1034
J. Bromley <jay@ai.co>
2019-09-18 12:01:14 -0700

    Added a sample test.
    Figuring out how using Clojure project tests work.

--- fwpd/test/fwpd/core_test.clj
+++ fwpd/test/fwpd/core_test.clj
@@ -2,6 +2,11 @@
   (:require [clojure.test :refer :all]
             [fwpd.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
+(deftest suspect-name-test
+  (testing "Convert list of suspect records to a list of names"
+    (is (= (find-suspects filename 3)
+           '("Edward Cullen" "Jacob Black" "Carlisle Cullen")))))
+
+(deftest failing-test
+  (testing "This test always fails."
     (is (= 0 1))))
