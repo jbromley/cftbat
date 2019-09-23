120433440c1dd80d7c1bf2d49d9c5a21e558e84d
J. Bromley <jay@ai.co>
2019-09-23 16:27:22 -0700

    Added pegthing project.

---
+++ pegthing/src/pegthing/core.clj
@@ -0,0 +1,44 @@
+(ns pegthing.core
+  (require [clojure.set :as set])
+  (:gen-class))
+
+(declare successful-move prompt-move game-over query-rows)
+
+(defn tri*
+  "Generates a lazy sequence of triangular numbers."
+  ([] (tri* 0 1))
+  ([sum n]
+   (let [new-sum (+ sum n)]
+     (cons new-sum (lazy-seq (tri* new-sum (inc n)))))))
+
+(def tri (tri*))
+
+(defn triangular?
+  "Is the number triangular?"
+  [n]
+  (= n (last (take-while #(>= n %) tri))))
+
+(defn row-tri
+  "The triangular number at the end of row n."
+  [n]
+  (last (take n tri)))
+
+(defn row-num
+  "Returns the row number the position belongs to."
+  [pos]
+  (inc (count (take-while #(> pos %) tri))))
+
+(defn connect
+  "Form a mutual connection between two positions."
+  [board max-pos pos neighbor destination]
+  (if (<= destination max-pos)
+    (reduce (fn [new-board [p1 p2]]
+              (assoc-in new-board [p1 :connections p2] neighbor))
+            board
+            [[pos destination] [destination pos]])
+    board))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
